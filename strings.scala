b0be3a0054b9f3e96cd9b9ab0da4b9a96068c037
celliott1 <celliott1@luc.edu>
2015-04-20 04:45:47 +0000

    Finished

--- strings.scala
+++ strings.scala
@@ -1,31 +1,39 @@
 import scala.math.min
 
 object strings  {
-
-/*
-
+    /*
    Write a function that accepts a sentence (string), and returns whether
    it represents a *declarative* sentence (i.e. ending in a period),
    *interrogatory* sentence (ending in a question mark), or an
    *exclamation* (ending in exclamation point) or is not a sentence
    (anything else).
-
    This may be the first time you write a conditional
    statement. (This needs the next chapter.)
    It makes sense to only make small changes at once and build
    up to final code. First you might just code it to check if a sentence is
    declarative or not. Then remember you can test further cases with
    ``else if (...)``.
-
    The function should return the string "interrogative", "declarative", or
    "exclamatory" for ?, ., or !; otherwise, it should return "unknown".
  */
 
 
+
    def getSentenceType(sentence : String) : String = {
      
-      "unknown"
-   }
+        if (sentence.endsWith(".")) {
+            "Declarative"
+        }
+        else if (sentence.endsWith("!")) {
+            "Exclamatory"
+        }
+        else if(sentence.endsWith("?")) {
+            "Interrogative"
+        }else {
+            "Unknown"
+        }
+}
+   
 
 /*
    Given a name as input.  Assume first and last names
@@ -40,8 +48,17 @@ object strings  {
  */
 
    def getFormattedName(name : String) : String = {
-     
-      "name not calculated yet"
+        var namearray = name.split(" ")
+        var arraylength = namearray.length
+       if(name.contains(" ")) {
+         if(arraylength == 2){
+                (namearray(1)+ ", " + namearray(0))
+            }
+           else if(arraylength == 3){
+                (namearray(namearray.length-1)+ ", " + namearray(0) +" "+ namearray(1))
+            }else{(name)}
+        }else {
+            (name)
+        }
    }
-
 }
